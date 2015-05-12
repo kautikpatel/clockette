7c3fff05f363300e70926cadfa7ad9250f310086
Thomas Le Jeune <thomas.lejeune@dailymotion.com>
2015-05-11 18:07:20 -0700

    Move ClocketteApp in .jsx (better to lint with sublimetext-babel)

---
+++ src/components/ClocketteApp.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react/addons';
+const ReactTransitionGroup = React.addons.TransitionGroup;
+
+// CSS
+import 'normalize.css';
+import '../styles/main.css';
+
+const ClocketteApp = React.createClass({
+  render: function() {
+    return (
+      <p>It's working!</p>
+    );
+  }
+});
+
+export default ClocketteApp;
