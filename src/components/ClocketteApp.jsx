4411ae6cc79111e683735a9fb24c360fd47d5a7b
Thomas Le Jeune <thomastheyoung@gmail.com>
2015-05-14 14:34:44 -0700

    Add routeHandler in App, application global timestamp state + interval to refresh it naively (so far)

--- src/components/ClocketteApp.jsx
+++ src/components/ClocketteApp.jsx
@@ -1,17 +1,40 @@
 'use strict';
 
 import React from 'react';
+import { RouteHandler } from 'react-router';
 
-// CSS
 import 'normalize.css';
 import 'styles/main.css';
 
+
 const ClocketteApp = React.createClass({
+
+  getInitialState() {
+    this.interval = null;
+
+    return {
+      ts: Date.now()
+    };
+  },
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        ts: Date.now()
+      });
+    }, 1000 * 30);
+  },
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+
   render() {
     return (
-      <p>It's working!</p>
+      <RouteHandler ts={this.state.ts}/>
     );
   }
+
 });
 
 export default ClocketteApp;
