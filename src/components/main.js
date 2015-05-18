61e0d50d209f66bc41916758095db957c752507b
Thomas Le Jeune <thomastheyoung@gmail.com>
2015-05-17 21:24:40 -0700

    Add AddTimezoneScreen route

--- src/components/main.js
+++ src/components/main.js
@@ -6,10 +6,12 @@ var Router = require('react-router');
 var Route = Router.Route;
 
 var HomeScreen = require('./HomeScreen');
+var AddTimezoneScreen = require('./AddTimezoneScreen');
 
 var Routes = (
   <Route handler={ClocketteApp}>
     <Route name="/" handler={HomeScreen}/>
+    <Route name="/timezones" handler={AddTimezoneScreen}/>
   </Route>
 );
 
