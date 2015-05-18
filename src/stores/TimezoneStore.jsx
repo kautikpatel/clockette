4dc37bcaf894a31cb4e84fc9aca947203fb6a541
Thomas Le Jeune <thomastheyoung@gmail.com>
2015-05-17 22:58:45 -0700

    Add method for each List construction step ; Add onSearchByName callback

--- src/stores/TimezoneStore.jsx
+++ src/stores/TimezoneStore.jsx
@@ -1,38 +1,54 @@
 'use strict';
 
-import Reflux from 'reflux';
 import Immutable from 'immutable';
+import Reflux from 'reflux';
 import Moment from 'moment-timezone';
+import UserActions from 'actions/UserActionCreators';
 
 
 const TimezoneStore = Reflux.createStore({
 
+  listenables: UserActions,
+
   init() {
     const meta = require('moment-timezone/data/meta/latest.json');
     const now = Date.now();
-    const zoneList = Object.keys(meta.zones)
-      .map((zone) => {
-        const name = zone.split('/')
-          .pop()
-          .replace(/_/gi, ' ')
-          .toLowerCase();
 
-        return {
-          zone,
-          name,
-          offset: Moment.tz.zone(zone).offset(now),
-        };
-      })
-      .sort((a, b) => {
-        return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
-      });
-
-    this.data = Immutable.List(zoneList);
+    this.data = Immutable.List(
+      Object.keys(meta.zones)
+        .map(this._createZone.bind(this, now))
+        .sort(this._sortByName)
+    );
   },
 
-  searchByName(name) {
-    const regex = new RegExp(name, 'i');
-    return this.data.filter((zone) => regex.test(zone.name));
+  _createZone(now, zone, index) {
+    const name = zone.split('/')
+      .pop()
+      .replace(/_/gi, ' ')
+      .toLowerCase();
+
+    return {
+      zone,
+      name,
+      offset: Moment.tz.zone(zone).offset(now),
+    };
+  },
+
+  _sortByName(a, b) {
+    return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
+  },
+
+  onSearchByName(query) {
+    const regexp = new RegExp('^' + query, 'i');
+    let results;
+
+    if (!query.length) {
+      results = this.data.clear();
+    }
+    else {
+      results = this.data.filter(zone => regexp.test(zone.name));
+    }
+    this.trigger('searchByName', {query, results});
   }
 
 });
