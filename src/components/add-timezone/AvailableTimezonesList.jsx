c39b0ce089553ce5591bffd1f52e67ab9d3954bf
Thomas Le Jeune <thomastheyoung@gmail.com>
2015-06-22 09:25:53 -0700

    Emulate a very simple infinite scrolling list

--- src/components/add-timezone/AvailableTimezonesList.jsx
+++ src/components/add-timezone/AvailableTimezonesList.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react/addons';
 import Immutable from 'immutable';
+import _ from 'lodash';
 
 import TimezoneListRow from './TimezoneListRow';
 
@@ -16,11 +17,66 @@ const AvailableTimezonesList = React.createClass({
   mixins: [PureRenderMixin],
 
   getInitialState() {
+    this.list = TimezoneStore.getSeq().filter(
+      (timezone) => !UserStore.data.contains(timezone)
+    ).cacheResult();
+    this.isBuilding = false;
+
     return {
-      list: TimezoneStore.getSeq().filter(
-        (timezone) => !UserStore.data.contains(timezone)
-      ).toArray()
+      elements: this.buildElements(0, 20),
     };
+  },
+
+  componentDidMount() {
+    const _window = window;
+    const containerNode = this.refs.container.getDOMNode();
+    const containerRect = containerNode.getBoundingClientRect();
+    const firstChildNode = containerNode.firstChild;
+    const firstChildRect = firstChildNode.getBoundingClientRect();
+
+    const pageHeight = (firstChildRect.height) * 10;
+
+    this.onScroll = _.throttle((e) => {
+      if (this.isBuilding === true) {
+        return;
+      }
+      if (this.state.elements.length >= this.list.size) {
+        document.removeEventListener('scroll', this.onScroll);
+        return;
+      }
+
+      let page = parseInt((this.state.elements.length / 20) - 1, 10);
+      let trigger = _window.scrollY > ((page * pageHeight) + containerRect.top);
+      if (trigger) {
+        this.isBuilding = true;
+        this.setState({
+          elements: this.buildElements(0, this.state.elements.length + 20),
+        });
+      }
+    }, 200);
+    document.addEventListener('scroll', this.onScroll);
+  },
+
+  componentDidUpdate() {
+    this.isBuilding = false;
+  },
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.onScroll);
+  },
+
+  renderElement(timezone) {
+    return (
+      <TimezoneListRow key={'available.' + timezone.zone}
+        className="AddTimezoneScreen__timezoneListRow"
+        timezone={timezone}
+        ts={this.props.ts}
+        selected={false}/>
+    );
+  },
+
+  buildElements(start, end) {
+    return this.list.slice(start, end).map(this.renderElement).toArray();
   },
 
   render() {
@@ -28,26 +84,7 @@ const AvailableTimezonesList = React.createClass({
 
     return (
       <div className="AddTimezoneScreen__availableTimezones" ref="container">
-        {this.state.list.map((timezone) => {
-          const index = timezone.name[0];
-          let data = null;
-
-          if (indexes.indexOf(index) === -1) {
-            indexes = indexes.push(index);
-            data = {
-              'data-index': index
-            };
-          }
-
-          return (
-            <TimezoneListRow key={'available.' + timezone.zone}
-              className="AddTimezoneScreen__timezoneListRow"
-              timezone={timezone}
-              ts={this.props.ts}
-              selected={false}
-              data={data}/>
-          );
-        })}
+        { this.state.elements }
       </div>
     );
   }
